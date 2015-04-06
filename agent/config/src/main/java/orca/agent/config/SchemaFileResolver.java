133e1f3ff16b44d4cd855cd7ad5db77586918b34
Ilya Baldin <ibaldin@users.noreply.github.com>
2015-04-06 21:29:34 +0000

    Importing NA2

---
+++ agent/config/src/main/java/orca/agent/config/SchemaFileResolver.java
@@ -0,0 +1,49 @@
+package orca.agent.config;
+
+import org.slf4j.Logger;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
+
+/***
+ * Custom resolver for schema files contained in different packages.
+ * Used for XML/XSD validation
+ * @author ibaldin
+ *
+ */
+public class SchemaFileResolver implements LSResourceResolver {
+        private DOMImplementationLS dls;
+        private ClassLoader cll;
+        private String pkgPath;
+        private Logger l;
+
+        /***
+         * Needs a class loader of the class to which the resource belongs
+         * @param cll
+         * @throws IllegalAccessException
+         * @throws ClassNotFoundException
+         * @throws InstantiationException
+         */
+        SchemaFileResolver(ClassLoader cll, String pkgPath, Logger log) throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+                this.cll = cll;
+                this.pkgPath = pkgPath;
+                this.l = log;
+                DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+                dls = (DOMImplementationLS)registry.getDOMImplementation("LS");
+        }
+
+        @Override
+        public LSInput resolveResource(String type, String ns, String pubId,
+                        String sysId, String baseURI) {
+                l.debug("Trying to resolve " + 
+                                type + ", " + ns + ", " + pubId + ", " + sysId + ", " + baseURI + " in package " + pkgPath);
+                // from http://www.java.net/forum/topic/java-web-services-and-xml/jaxp/
+                // schemafactory-problem-compiling-multiple-schemas-same-na-0
+
+                LSInput lsi = dls.createLSInput();
+                lsi.setByteStream(cll.getResourceAsStream(pkgPath.replace('.', '/') + "/" + sysId));
+                lsi.setSystemId(sysId);
+                return lsi;
+        }
+}
---
+++ agent/core/src/main/java/orca/agent/App.java
@@ -0,0 +1,13 @@
+package orca.agent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
