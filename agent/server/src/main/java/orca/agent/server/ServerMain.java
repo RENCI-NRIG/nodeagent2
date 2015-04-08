a4d0c531135382862fd3039e8e354f89d4c849d3
Ilya Baldin <ibaldin@users.noreply.github.com>
2015-04-08 18:40:13 +0000

    Sorted out specifying logging configuration externally

--- agent/server/src/main/java/orca/agent/server/ServerMain.java
+++ agent/server/src/main/java/orca/agent/server/ServerMain.java
@@ -1,22 +1,75 @@
 package orca.agent.server;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import orca.agent.config.Config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class ServerMain {
-	
-	public static void main(String[] argv) {
-		Log l = LogFactory.getLog("serverMain");
-		try {
-			Config.initialize(argv[0]);
-		} catch (Exception e) {
-			l.error(e);
+	private static final String DEFAULT_LOG4J="orca/agent/server/log4j-default.properties";
+
+	// SPRING gets its configuration from externally specified (via SPRING_CONFIG_LOCATION env. variable)
+	// in it there is a 'logging.config' variable that specified the log4j config file that is used by
+	// NA2 as well.
+	private static class NA2Initializer implements ApplicationContextInitializer<GenericApplicationContext> {
+		String[] argv;
+
+		public NA2Initializer(String[] a) {
+			argv = a;
 		}
-		SpringApplication.run(ServerMain.class, argv);
+
+		public void initialize(GenericApplicationContext ac) {
+
+			// init logging
+			String loggingConfig = ac.getEnvironment().getProperty("logging.config");
+			boolean defaultConfig = true;
+
+			if (loggingConfig != null) {
+				File lpf = new File(loggingConfig);
+				if (lpf.exists()) { 
+					try {
+						PropertyConfigurator.configure(new FileInputStream(lpf));
+						defaultConfig = false;
+					} catch(FileNotFoundException fnfe) {
+						;
+					}
+				}
+				
+				if (defaultConfig) 
+					PropertyConfigurator.configure(ServerMain.class.getClassLoader().getResourceAsStream(DEFAULT_LOG4J));
+
+				Log l = LogFactory.getLog("serverMain");
+				l.info("NA2 using " + (defaultConfig ? "built-in log4j configuration" : "user-specified configuration in " + loggingConfig));
+
+				try {
+					l.info("Initializing config parser");
+					Config.initialize(argv[0]);
+				} catch (Exception e) {
+					l.error(e);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] argv) {
+		// start up spring
+		SpringApplication spring = new SpringApplication(ServerMain.class);
+
+		NA2Initializer na2 = new NA2Initializer(argv);
+
+		spring.addInitializers(na2);
+
+		spring.run(argv);
+
 	}
 }
