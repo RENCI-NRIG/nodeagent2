fe0dc03c6697e8c6897f2b6651554d37617443b8
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2015-04-09 19:01:28 +0000

    Some refactoring

---
+++ orca-external/node-agent2/trunk/agent/config/src/main/java/orca/nodeagent2/agent/config/Config.java
@@ -0,0 +1,47 @@
+package orca.nodeagent2.agent.config;
+
+import java.util.List;
+
+import orca.nodeagent2.agent.config.xsd.PluginType;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Config {
+
+	Log l;
+	ConfigParser cp;
+	static String fname = null;
+	
+	public static Config instance = null;
+	
+	private Config() throws Exception {
+		l = LogFactory.getLog("config");		
+		if (fname == null) {
+			throw new Exception("Configuration filename not specified");
+		}
+		cp = new ConfigParser(fname);
+		
+		if (!cp.isInitialized()) 
+			throw new Exception("Config parser was not initialized");
+	}
+	
+	public static Config getInstance() throws Exception {
+		if (instance == null)
+			throw new Exception("Config not initialized");
+		return instance;
+	}
+	
+	public static void initialize(String c) throws Exception {
+		fname = c;
+		instance = new Config();
+	}
+	
+	public String getPassword() {
+		return cp.getPassword();
+	}
+	
+	public List<PluginType> getPlugins() {
+		return cp.getPlugins();
+	}
+}
---
+++ orca-external/node-agent2/trunk/agent/core/src/main/java/orca/nodeagent2/agent/core/App.java
@@ -0,0 +1,13 @@
+package orca.nodeagent2.agent.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
