22daae9028f22f05e93bd16f532a631fef0c855b
ibaldin <ibaldin@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2015-04-19 20:49:31 +0000

    Added thread pool for servicing renews; added example of server-side SSL support

---
+++ orca-external/node-agent2/trunk/agent/server/src/main/java/orca/nodeagent2/agent/server/renew/RenewExpiringThread.java
@@ -0,0 +1,77 @@
+package orca.nodeagent2.agent.server.renew;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import orca.nodeagent2.agent.config.Config;
+import orca.nodeagent2.agent.core.PluginsRegistry;
+import orca.nodeagent2.agent.server.persistence.ScheduleEntry;
+import orca.nodeagent2.agent.server.persistence.SchedulePersistence;
+import orca.nodeagent2.agentlib.PluginException;
+import orca.nodeagent2.agentlib.PluginReturn;
+import orca.nodeagent2.agentlib.ReservationId;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RenewExpiringThread implements Runnable {
+
+	private final Log l = LogFactory.getLog("renewExpiringThread");
+
+	private final SchedulePersistence sp;
+
+	private static final int MAX_RENEW_SERVICE_THREADS = 10;
+	static ExecutorService renewThreadFactory = Executors.newFixedThreadPool(MAX_RENEW_SERVICE_THREADS);
+
+	public RenewExpiringThread(SchedulePersistence sp) {
+		this.sp = sp;
+	}
+
+	@Override
+	public void run() {
+		Thread.currentThread().setName("renewPeriodicThread");
+		List<ScheduleEntry> expired = sp.findExpiredEntries();
+		l.info("There are " + expired.size() + " expired entries");
+		for(final ScheduleEntry e: expired) {
+			l.info("Spawning a thread to renew " + e.getName() + " reservation " + e.getReservationId());
+			renewThreadFactory.submit(new Thread() {
+				public void run() {
+					try {
+						Thread.currentThread().setName("renew" + e.getName() + "/" + e.getReservationId() + "Thread");
+						
+						// compute new expiry time as now + plugin period
+						Calendar future = Calendar.getInstance();
+						future.add(Config.getInstance().getSchedulePeriodCalendarUnit(e.getName()), 
+								Config.getInstance().getSchedulePeriod(e.getName()));
+						l.info("RENEW call to " + e.getName() + " reservation " + e.getReservationId() + ", setting new deadline to " + future.getTime());	
+
+						// invoke renew
+						PluginReturn pr = PluginsRegistry.getInstance().renew(e.getName(), 
+								new ReservationId(e.getReservationId()), future.getTime(), 
+								e.getInProperties(), e.getJoinProperties());
+
+						// merge original join properties with properties returned by renew
+						e.getJoinProperties().putAll(pr.getProperties());
+
+						// update the database with a new schedule entry
+						l.info("Updating the database for " + e.getName() + " existing reservation " + e.getReservationId());
+						sp.saveRenewDeadline(e.getName(), future.getTime(), pr.getResId(), e.getInProperties(), e.getJoinProperties());
+
+						// remove old entry
+						l.debug("Removing old entry from the database");
+						sp.removeEntries(Arrays.asList(e));
+						
+						l.info("exiting");
+					} catch (PluginException pe) {
+						l.error("Unable to renew plugin " + e.getName() + " reservation " + e.getReservationId());
+					} catch (Exception ee) {
+						l.error("Unable to renew plugin " + e.getName() + " reservation " + e.getReservationId());
+					}
+				}
+			});
+		}
+	}
+}
